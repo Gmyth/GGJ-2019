0a6a939b901e1a15f732d8b8c5601988add0f92a
Zheyu Zhang (RIT Student) <zz9691@ad.rit.edu>
2019-01-25 21:43:50 -0500

    Sync player.cs

---
+++ Assets/CSharp/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    /// <summary>
+    /// The name of the player, which is set at the beginning of the game
+    /// </summary>
+    public string Name { get; private set; }
+
+    /// <summary>
+    /// The score that has been earned by the player
+    /// </summary>
+    public int Score { get; private set; }
+
+    /// <summary>
+    /// The number of pillows carried by the player
+    /// </summary>
+    public int Pillow { get; private set; }
+
+    private Player()
+    {
+        Name = "";
+        Score = 0;
+        Pillow = 1;
+    }
+}
---
+++ Assets/CSharp/UI/UIWindow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public abstract class UIWindow : MonoBehaviour
+{
+    public virtual void OnOpen(params object[] args) {}
+    public virtual void OnClose() {}
+
+    public virtual void Redraw() {}
+    public virtual void UpdateAll() {}
+
+    public virtual void Close()
+    {
+        UIManager.Singleton.Close(GetType().Name);
+    }
+}
