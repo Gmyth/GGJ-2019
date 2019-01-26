979fbaf00fe07db8299b23caec1affef82e097e0
Haoyu Guo (RIT Student) <hg7364@ad.rit.edu>
2019-01-25 23:04:03 -0500

    1231

---
+++ Assets/CSharp/Pillow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pillow : MonoBehaviour {
+    public bool Picked;
+    public bool Throwed; // if the pillow is throwed, then do damage
+
+    private GameObject holder;
+	// Use this for initialization
+	void Start () {
+        Picked = false;
+    }
+	
+	// Update is called once per frame
+	void Update (){
+        if (Picked) {
+            // pillow follow
+            transform.position = Vector3.Lerp(transform.position, holder.transform.position, Time.time);
+        }
+    }
+
+    public void Pick(GameObject player){
+        Picked = true;
+        holder = player;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Throwed)
+            {
+                // doing dmg TODO
+                Throwed = false;
+            }
+        }
+    }
+}
--- Assets/CSharp/Player.cs
+++ Assets/CSharp/Player.cs
@@ -2,11 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-    /// <summary>
-    /// The name of the player, which is set at the beginning of the game
-    /// </summary>
+public class Player : MonoBehaviour {
     public string Name { get; private set; }
 
     /// <summary>
@@ -14,15 +10,128 @@ public class Player : MonoBehaviour
     /// </summary>
     public int Score { get; private set; }
 
+    // Use this for initialization
+    [SerializeField]private float speed;
+    [SerializeField]private float jumpSpeed;
+    [SerializeField]private float gravity;
+
+
+    [SerializeField] private float minThrowPower;
+    [SerializeField] private float maxThrowPower;
+
     /// <summary>
     /// The number of pillows carried by the player
-    /// </summary>
-    public int Pillow { get; private set; }
+    /// </summary
+    private int numPillowHold;
+    private List<Pillow> Pillows;
+    private Pillow[] Ammo;
+    private float emPower;
 
-    private Player()
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    void Start()
     {
-        Name = "";
-        Score = 0;
-        Pillow = 1;
+        numPillowHold = 0;
+        controller = GetComponent<CharacterController>();
+        Ammo = new Pillow[2];
+        Pillows = new List<Pillow>();
+        emPower = 0.0f;
+        // let the gameObject fall down
+        gameObject.transform.position = new Vector3(0, 5, 0);
+    }
+
+    void Update()
+    {
+        // all input interface
+        if (controller.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        if (Input.GetButton("Pick"))
+        {
+            if (numPillowHold >= 2)
+            {
+                // exceed the number that one player can hold 
+                ThrowDrop();
+            }
+            else {
+                print("Pick!");
+                PickUp();
+            }
+        }
+
+        if (Input.GetButton("Throw") && emPower < 0.1f)
+        {
+            if (numPillowHold > 0) {
+                // start to empower the throw
+                emPower = minThrowPower;
+            }
+            // do nothing if no pillow
+        }
+        emPower = emPower < maxThrowPower? emPower + 0.01f : emPower;
+        if (Input.GetKeyUp("joystick button 5") && emPower > 0.1f)
+        {
+            // key released throw
+            emPower = 0.0f;
+            // throw!!!!
+            // TODO: pillow class
+        }
+        // Apply gravity
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+    public void ThrowDrop() {
+
+    }
+
+    public void PickUp() {
+            for (int i = 0; i < Pillows.Count; i++) {
+                var temp = Pillows[i];
+                if (!temp.Picked) {
+                    temp.Pick(gameObject);
+                    numPillowHold++;
+                    break;
+                }
+            }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        print("pillow found");
+        if (other.gameObject.tag == "Pillow") {
+            print("pillow found");
+            var temp = other.gameObject.GetComponent<Pillow>();
+            if (temp.Throwed){
+                // doing dmg TODO
+
+            }else{
+                // sign into queue for pick up
+                if (!temp.Picked) { Pillows.Add(temp);}
+            }
+        }   
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Pillow")
+        {
+            var temp = other.gameObject.GetComponent<Pillow>();
+            if (Pillows.Contains(temp)) { 
+                // sign out queue
+                Pillows.Remove(temp);
+            }
+        }
     }
 }
