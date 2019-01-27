7698948e255f73833a9081498b274701f600552e
Yiyan Wang <robinoneword@outlook.com>
2019-01-26 20:04:55 -0500

    Item Speed Change Done

---
+++ Assets/CSharp/LevelMechanics/ItemSpeedChange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ItemSpeedChange : MonoBehaviour {
+    float currCountdownValue;
+    GameObject player = null;
+    [SerializeField] private float duration = 10f;
+    [SerializeField] private float speedfactor = 1f;
+    void Awake()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            gameObject.SetActive(false);
+            player = other.gameObject;
+        }
+        player.GetComponent<Player>().SetSpeed(speedfactor);
+        StartCoroutine(StartCountdown(duration));
+        
+    }
+    public IEnumerator StartCountdown(float countdownValue = 10)
+    {
+        currCountdownValue = countdownValue;
+        while (currCountdownValue > 0)
+        {
+            Debug.Log("Countdown: " + currCountdownValue);
+            yield return new WaitForSeconds(1.0f);
+            currCountdownValue--;         
+        }
+        player.GetComponent<Player>().ResetSpeed();
+    }
+}
