e74fb59e3c1f25c66f4cf1e6e9e8d2e6e0d333c9
Yiyan Wang <robinoneword@outlook.com>
2019-01-27 13:46:12 -0500

    Alter Swimming Pool

--- Assets/CSharp/LevelMechanics/SwimmingPool.cs
+++ Assets/CSharp/LevelMechanics/SwimmingPool.cs
@@ -2,26 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwimmingPool : MonoBehaviour {
+public class SwimmingPool : MonoBehaviour
+{
 
     [SerializeField] private float reduceSpeedMultiplier;
+    private bool SoundActivate = false;
+    private bool isPlaying = false;
+    // Use this for initialization
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SoundActivate == true)
+        {
+            if (!isPlaying)
+            {
+                AudioManager.Instance.PlaySoundEffect("WalkingInPool", true, false);
+                isPlaying = true;
+            }
+        }
+        else
+        {
+            AudioManager.Instance.StopSoundEffect("WalkingInPool", true);
+            isPlaying = false;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
+            AudioManager.Instance.PlaySoundEffect("EnterWater");
+            SoundActivate = true;
             //Reduce player's speed
-            other.transform.parent.GetComponent<Player>().SetSpeed(reduceSpeedMultiplier);
+            other.GetComponent<Player>().SetSpeed(reduceSpeedMultiplier);
             //Slow down the pillow pitch speed
 
         }
@@ -31,8 +49,9 @@ public class SwimmingPool : MonoBehaviour {
     {
         if (other.tag == "Player")
         {
+            SoundActivate = false;
             //Reset player's speed
-            other.transform.parent.GetComponent<Player>().ResetSpeed();
+            other.GetComponent<Player>().ResetSpeed();
         }
     }
 
